140a08710c9a81565c011b37531cca0bcc4fdb92
redgyuf <faramirex070@gmail.com>
2017-01-17 14:06:45 +0100

    person - default energy, social level added

--- person.py
+++ person.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level, social_life):
+    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level = 75, social_life = 20):
         self.first_name = first_name
         self.last_name = last_name
         self.year_of_birth = year_of_birth
