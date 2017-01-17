e197ac44edad1643d9d935b155b3e24a025fdaa8
redgyuf <faramirex070@gmail.com>
2017-01-17 14:03:01 +0100

    person class, init done

--- person.py
+++ person.py
@@ -1,2 +1,8 @@
 class Person:
-    pass
+    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level, social_life):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = year_of_birth
+        self.gender = gender
+        self.energy_level = energy_level
+        self.social_life = social_life
