ec26e75a4501f80e5554e6d0eddfb9b5ee803c82
Bosnyák Imre Dávid <suti211@gmail.com>
2017-01-18 14:32:04 +0100

    implemented mentors_room class

--- mentors_room.py
+++ mentors_room.py
@@ -1,10 +1,15 @@
+from student import Student
+
+
 class MentorsRoom:
 
     def __init__():
-        pass
+        self.bfa_status = None
 
     def do_a_bfa(self, student):
-        pass
+        student.energy_level -= 20
+        self.bfa_status = True
 
     def do_a_meeting(self, participants):
-        pass
+        for mentor in participants:
+            mentor.energy_level -= 20
