87c92f9113508490ff0245f6db7339a0628a538c
redgyuf <faramirex070@gmail.com>
2017-01-18 13:19:21 +0100

    implemented lounge class

---
+++ lounge.py
@@ -0,0 +1,20 @@
+class Lounge:
+    def __init__(self, darts_Skill_level, playstation_skill_level):
+        self.darts_skill_level = darts_Skill_level
+        self.playstation_skill_level = playstation_skill_level
+
+    def play(self, game_name, time_spent):
+        if game_name == "darts":
+            self.darts_skill_level += 5 * time_spent
+        elif game_name == "playstation":
+            self.playstation_skill_level += 5 * time_spent
+        else:
+            pass
+            #throw error
+
+    def nap_in_bean_bags(person, time_spent):
+        person.energy_level += 2 * time_spent
+
+    def watch_random_video_on_tv(person, time_spent):
+        person.energy_level += 1 * time_spent
+        person.social_life += 2 * time_spent
