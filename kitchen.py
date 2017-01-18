cd214e46f41d8010ea39957ebb68d07f718c232b
redgyuf <faramirex070@gmail.com>
2017-01-18 13:30:32 +0100

    implemented kitchen class

---
+++ kitchen.py
@@ -0,0 +1,13 @@
+class Kitchen():
+    def __init__(self, participants, hunger_level):
+        self.participants = participants
+        self.hunger_level = hunger_level
+
+    def drink_coffee(person):
+        person.energy_level += 20
+
+    def eat(self):
+        self.hunger_level += 30
+
+    def random_talk(person):
+        person.social_life += 15
