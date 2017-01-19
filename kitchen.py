80e23d19d8f0f8259348c4840f63528d7c7b7e36
redgyuf <faramirex070@gmail.com>
2017-01-19 15:06:32 +0100

    Revert "kitchen methods now contains prints aswell"
    This reverts commit d7775f0262dce7a966e98cc1b015caadf34be289.

--- kitchen.py
+++ kitchen.py
@@ -3,16 +3,11 @@ class Kitchen():
     @staticmethod
     def drink_coffee(person):
         person.energy_level += 20
-        print(person.first_name + " " + person.last_name + "'s energy level increased to " + str(person.energy_level))
-
 
     @staticmethod
     def eat(person):
-        person.hunger_level -= 30
-        print(person.first_name + " " + person.last_name + "'s hunger level decreased to " + str(person.hunger_level))
-
+        person.hunger_level += 30
 
     @staticmethod
     def random_talk(person):
         person.social_life += 15
-        print(person.first_name + " " + person.last_name + "'s Social Life level increased to " + str(person.social_life))
