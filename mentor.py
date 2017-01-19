3d7558ff8842f444829b3ac580121b70f651d319
redgyuf <faramirex070@gmail.com>
2017-01-19 11:49:03 +0100

    mentor, student class corrected properly to parent class

--- mentor.py
+++ mentor.py
@@ -4,24 +4,28 @@ from person import Person
 
 class Mentor(Person):
 
-    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level, social_life, mentor_name):
-        super().__init__(first_name, last_name, year_of_birth,
-                         gender, energy_level, social_life)
+    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level = 75, social_life = 20,
+                 darts_skill_level = 0, playstation_skill_level = 0, hunger_level = 0, blood_alcohol_level = 0,
+                 mentor_name = ""):
+        super().__init__(first_name, last_name, year_of_birth, gender, energy_level,
+                 social_life, darts_skill_level, playstation_skill_level, hunger_level, blood_alcohol_level)
         self.mentor_name = mentor_name
 
-    def do_presentation(self, students_object_list):
-        for student in students_object_list:
-            student.energy_level -= 30
-            student.knowledge_level += 15
-            self.energy_level -= 20
+    @staticmethod
+    def do_presentation(person):
+            person.energy_level -= 30
+            person.knowledge_level += 15
 
     @classmethod
     def create_by_csv(cls):
         with open('data/mentors.csv') as csvfile:
-            mentors_list = []
+            mentors_obj_list = []
             data = csv.reader(csvfile)
             for mentor_row_raw_data in data:
-                first_name, last_name, year_of_birth, gender, energy_level, social_life, mentor_name = mentor_row_raw_data
-                mentors_list.append(Mentor(first_name, last_name, int(
-                    year_of_birth), gender, int(energy_level), int(social_life), mentor_name))
-        return mentors_list
+                first_name, last_name, year_of_birth, gender, energy_level, \
+                social_life, darts_skill_level, playstation_skill_level, hunger_level, blood_alcohol_level, mentor_name = mentor_row_raw_data
+
+                mentors_obj_list.append(Mentor(first_name, last_name, int(year_of_birth), gender, int(energy_level),
+                                           int(social_life), int(darts_skill_level), int(playstation_skill_level), int(hunger_level),
+                                           int(blood_alcohol_level), mentor_name))
+        return mentors_obj_list
--- student.py
+++ student.py
@@ -10,10 +10,16 @@ class Student(Person):
     GENDER = 3
     ENERGY = 4
     SOCIAL = 5
+    DARTS_SK = 6
+    PS_SK = 7
+    HUNGER = 8
+    BA_LEV = 9
 
-    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level, social_life, knowledge_level=50):
-        super().__init__(first_name, last_name, year_of_birth,
-                         gender, energy_level, social_life)
+    def __init__(self, first_name, last_name, year_of_birth, gender, energy_level = 75,
+                 social_life = 20, darts_skill_level = 0, playstation_skill_level = 0,
+                 hunger_level = 0, blood_alcohol_level = 0):
+        super().__init__(first_name, last_name, year_of_birth, gender, energy_level,
+                 social_life, darts_skill_level, playstation_skill_level, hunger_level, blood_alcohol_level)
 
     @classmethod
     def create_by_csv(cls):
@@ -27,6 +33,8 @@ class Student(Person):
 
         for entry in range(len(students)):
             student_objects.append(Student(students[entry][cls.FIRST_NAME], students[entry][cls.LAST_NAME], int(students[entry][
-                                   cls.BIRTH]), students[entry][cls.GENDER], int(students[entry][cls.ENERGY]), int(students[entry][cls.SOCIAL])))
+                                   cls.BIRTH]), students[entry][cls.GENDER], int(students[entry][cls.ENERGY]), int(students[entry][cls.SOCIAL]),
+                                           int(students[entry][cls.DARTS_SK]), int(students[entry][cls.PS_SK]), int(students[entry][cls.HUNGER]),
+                                           int(students[entry][cls.BA_LEV])))
 
         return student_objects
