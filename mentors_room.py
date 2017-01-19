47af9bdad4417406dd07263daed28d84853931f5
Bosnyák Imre Dávid <suti211@gmail.com>
2017-01-19 11:30:30 +0100

    modified file to new standards

--- mentors_room.py
+++ mentors_room.py
@@ -1,15 +1,11 @@
-from student import Student
-
-
 class MentorsRoom:
 
     def __init__(self):
         self.bfa_status = None
 
-    def do_a_bfa(self, student):
+    def do_a_bfa(self, person):
         student.energy_level -= 20
         self.bfa_status = True
 
-    def do_a_meeting(self, participants):
-        for mentor in participants:
-            mentor.energy_level -= 20
+    def do_a_meeting(person):
+        person.energy_level -= 10
